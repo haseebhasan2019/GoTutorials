be8ddc6f600d612698713298987fcec304756f59
haseebhasan2019 <haseebhasan2019@gmail.com>
2022-01-06 15:20:46 -0500

    Greetings Module and Error Handling

--- hello/hello.go
+++ hello/hello.go
@@ -2,10 +2,25 @@ package main
 
 import (
 	"fmt"
+	"log"
 
-	"rsc.io/quote"
+	"example.com/greetings"
 )
 
 func main() {
-	fmt.Println(quote.Go())
+	//Set properties of the predefined Logger:
+	// log entry prefix and a flag to disable printing
+	//the time, source file, and line number
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// Request a greeting message
+	message, err := greetings.Hello("John")
+	//If an error was returned, print it to the console and exit program
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//If no error was returned, print returned message to console
+	fmt.Println(message)
 }
